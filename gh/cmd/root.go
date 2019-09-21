658a482b91622dba710a889e78b9ee502d3c4035
HiroyukiYagihashi <h.yagihashi2424@icloud.com>
2019-09-22 05:29:58 +0900

    Change Deatails

--- gh/cmd/root.go
+++ gh/cmd/root.go
@@ -13,13 +13,8 @@ var cfgFile string
 
 var rootCmd = &cobra.Command{
   Use:   "gh",
-  Short: "A brief description of your application",
-  Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+  Short: "Command to use Git/Github easily.",
+  Long: `[WIP]`,
 
   	Run: func(cmd *cobra.Command, args []string) {
       branchName, _:= cmd.Flags().GetString("cop")
